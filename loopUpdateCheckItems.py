a1c0cfffb6d3d6f1a155e1b4df4200e223f3017f
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2019-10-04 17:22:02 -0700

    Added to the exercises

---
+++ loopUpdateCheckItems.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 15:43:59 2019
+
+@author: austin.schrader
+"""
+
+# loopUpdate.py
+
+def testNumber(num):
+    if num in [1, 3, 5]:
+        print("Thanks")
+    else:
+        print("Number is not 1, 3 or 5")
+        
+testNumber(3)
+testNumber(4)

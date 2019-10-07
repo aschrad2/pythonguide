79230e377435827c89af08c93bb51a4a45eb60a7
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2019-10-07 11:07:14 -0700

    Initialized the Jungle class

---
+++ ex1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 11:06:15 2019
+
+@author: austin.schrader
+"""
+
+# class declaration
+class Jungle:
+    pass
+
+# create object of class Jungle
+j = Jungle()
+print(j)
